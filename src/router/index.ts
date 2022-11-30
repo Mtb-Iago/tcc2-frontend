a21ff0ff68249c0fb7db68e5b6ba6f62b50b73c6
mtb-iago <iagooliveira09@outlook.com>
2022-11-29 23:30:27 -0300

    commit for adjust

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,10 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import CategoriesView from '../views/CategoriesView.vue'
 import PostsView from '../views/PostsView.vue'
-import LoginView from '../views/LoginView.vue'
 
 import Guard from '../services/auth-header'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/login',
-    name: 'login',
-    component: LoginView
-  },
   {
     path: '/',
     name: 'home',
